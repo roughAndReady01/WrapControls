72a5903ce6a0bf876b7c820c9e9e6cfa5586cde8
Haruki Hanagata <haruzo28@icloud.com>
2023-01-26 12:34:45 +0900

    Initial Commit

---
+++ WrapControls/WrapControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrapControlsApp.swift
+//  WrapControls
+//
+//  Created by 花形春輝 on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WrapControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
